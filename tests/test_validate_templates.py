9d241a9083d3e0956ccb4ec464c0e1bde32e7bbb
Guido Günther <agx@sigxcpu.org>
2017-09-27 13:08:36 +0200

    tests: Properly setup jinja2 env

--- tests/test_validate_templates.py
+++ tests/test_validate_templates.py
@@ -1,12 +1,16 @@
 import os
 
-from validate_templates import check_tree
+import validate_templates
+
+
+def setup():
+    validate_templates.env = validate_templates.setup_jinja2_env()
 
 
 def test_failure():
     dir = 'tests/data/invalid_template'
     assert os.path.exists(dir)
-    errors = check_tree(dir)
+    errors = validate_templates.check_tree(dir)
     assert len(errors) == 1
     assert errors[0].template == os.path.join(dir, 'bla.j2')
 
@@ -14,5 +18,5 @@ def test_failure():
 def test_filter():
     dir = 'tests/data/invalid_template'
     assert os.path.exists(dir)
-    errors = check_tree(dir, lambda x: x.endswith('/bla.j2'))
+    errors = validate_templates.check_tree(dir, lambda x: x.endswith('/bla.j2'))
     assert len(errors) == 0
